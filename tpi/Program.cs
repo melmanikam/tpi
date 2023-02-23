520b1e8836600beb025b941b73e80157d6b7a21f
Mel Manikam <ac111953@avcol.school.nz>
2023-02-24 09:47:55 +1300

    changed writeline

--- tpi/Program.cs
+++ tpi/Program.cs
@@ -10,7 +10,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    Console.WriteLine("Hello World");
+    Console.WriteLine("Bye World");
     app.UseHsts();
 }
 
